2e4b8b52e0d276dcc2f775af97bc2c7c67cdd1aa
johnadegun6 <125451917+johnadegun6@users.noreply.github.com>
2023-08-11 19:35:43 +0100

    toggle implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
+import {useState} from "react"
+
 function App() {
+  const [annually, setAnnually] = useState(false)
+
   return (
     <>
       <section className="p-5 lg:flex lg:flex-col items-center justify-center lg:h-screen ">
         <h1 className=" text-center text-slate-800 mb-8 text-4xl ">Our Pricing</h1>
+        <div class="toggle lg:mb-10">
+          <label class="name" >Annually</label>
+          <input 
+          type="checkbox" 
+          class="checkbox" 
+          id="checkbox" 
+          checked={annually} 
+          onChange={() => setAnnually(!annually)}
+          />
+          <label for="checkbox" class="label">
+            <div class="ball"></div>
+          </label>
+          <label class="name" >Monthly</label>
+        </div>
         <div className=" flex flex-wrap flex-row justify-center gap-0 text-center max-w-6xl mx-auto lg-gap-0 lg-flex-row rounded-md lg:w-full lg:flex-nowrap ">
           <article className=" p-8 rounded bg-white shadow ">
             <h2 className=" text-slate-800 text-xl mb-5 ">Basic</h2>
-            <h3 className=" flex items-center text-slate-700 text-5xl justify-center"><span className=" text-3xl mr-3">$</span>19.99</h3>
-            <h3 className=" flex items-center text-slate-700 text-5xl justify-center"><span className=" text-3xl mr-3">$</span>199.99</h3>
+
+            {annually && (
+            <h3 className=" flex items-center text-slate-700 text-5xl justify-center">
+              <span className=" text-3xl mr-3">$</span>19.99
+            </h3>)}
+            {! annually && (
+            <h3 className=" flex items-center text-slate-700 text-5xl justify-center">
+              <span className=" text-3xl mr-3">$</span>199.99
+            </h3>)}
+            
             <ul className=" flex justify-center flex-col my-8">
               <li className=" border-t py-3 border-slate-600  ">500 GB Storage</li>
               <li className=" border-t py-3 border-slate-600  "> 2 Users Allowed</li>
@@ -16,10 +42,19 @@ function App() {
             <button className=" linear-gradient btn text-white  py-3 w-full rounded-lg shadow uppercase cursor-pointer hover:border-2 border-2">Learn More</button>
           </article>
 
-          <article className="linear-gradient p-8 rounded shadow lg:scale-110 ">
+
+          <article className="linear-gradient p-8 rounded shadow ">   {/* lg:scale-80 */}
             <h2 className=" text-xl mb-5 text-white ">Professional</h2>
-            <h3 className=" flex items-center text-5xl justify-center  text-white"><span className=" text-3xl mr-3">$</span>24.99</h3>
-            <h3 className=" flex items-center text-5xl justify-center  text-white"><span className=" text-3xl mr-3">$</span>249.99</h3>
+
+            {annually && ( 
+            <h3 className=" flex items-center text-5xl justify-center  text-white">
+              <span className=" text-3xl mr-3">$</span>24.99
+            </h3>)}
+            {! annually && (
+            <h3 className=" flex items-center text-5xl justify-center  text-white">
+              <span className=" text-3xl mr-3">$</span>249.99
+            </h3>)}
+            
             <ul className=" flex justify-center flex-col my-8">
               <li className=" text-white border-t py-3 border-white ">1 GB Storage</li>
               <li className=" text-white border-t py-3 border-white " > 5 Users Allowed</li>
@@ -28,10 +63,19 @@ function App() {
             <button className=" bg-white text-slate-800 py-3 w-full rounded-lg shadow border-2 border-white uppercase cursor-pointer hover:bg-transparent hover:text-white transition-all duration-200 ">Learn More</button>
           </article>
 
+
           <article className=" p-8 bg-white rounded shadow ">
             <h2 className=" text-slate-800 text-xl mb-5 ">Master</h2>
-            <h3 className=" flex items-center text-slate-700 text-5xl justify-center"><span className=" text-3xl mr-3">$</span>39.99</h3>
-            <h3 className=" flex items-center text-slate-700 text-5xl justify-center"><span className=" text-3xl mr-3">$</span>399.99</h3>
+
+            {annually && (
+            <h3 className=" flex items-center text-slate-700 text-5xl justify-center">
+              <span className=" text-3xl mr-3">$</span>39.99
+            </h3>)}
+            {! annually && ( 
+            <h3 className=" flex items-center text-slate-700 text-5xl justify-center">
+              <span className=" text-3xl mr-3">$</span>399.99
+            </h3>)}
+           
             <ul className=" flex justify-center flex-col my-8">
               <li className=" border-t py-3 border-slate-600  ">2 GB Storage</li>
               <li className=" border-t py-3 border-slate-600  "> 10 Users Allowed</li>
@@ -39,11 +83,21 @@ function App() {
             </ul>
             <button className=" linear-gradient btn text-white  py-3 w-full rounded-lg shadow uppercase cursor-pointer hover:border-2 border-2">Learn More</button>
           </article>
+
+
         </div>
-      <div class="attribution">
-    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-    Coded by <a href="#">Adegun John Timilehin</a>.
-  </div>
+
+        <br />
+        <div class="attribution">
+          Challenge by {""}
+          <a href="https://www.frontendmentor.io?ref=challenge" 
+            target="_blank"
+            rel='noreferrer'
+          >
+            Frontend Mentor
+          </a>. 
+          . Coded by <a href="#"  target="_blank" rel='noreferrer'>Adegun John Timilehin</a>.
+        </div>
       </section>
     </>
   );
